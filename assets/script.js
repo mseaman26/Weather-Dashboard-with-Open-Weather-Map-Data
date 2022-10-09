7cdf63ce20f1a7ca3b99d2adc1b049d2e49402c7
michael seaman <mseaman26@gmail.com>
2022-10-08 20:05:02 -0700

    completed getting data for current day and 5-day forecast

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,6 @@ $(document).ready(function(){
             cityListDiv.innerHTML = ""
             return
         }
-        console.log("current city "+currentCityInput)
         displayedCities.push(currentCityInput)
         localStorage.setItem("displayed-cities", JSON.stringify(displayedCities))
         cityListDiv.innerHTML = ""
@@ -40,14 +39,12 @@ $(document).ready(function(){
         document.getElementById("city-list-div").innerHTML = ""
         //Document.getElementById("city-list-div").innerHTML = ""
         displayedCities = []
-        console.log(displayedCities)
         localStorage.setItem("displayed-cities", JSON.stringify(displayedCities))
         
     }
     function displayDate(){
         dateH2 = document.createElement("h2")
         dateH2.textContent = currentDate
-        console.log(dateH2)
         document.getElementById("current-weather-div").appendChild(dateH2)
     }
     function getIcon(){
@@ -64,14 +61,13 @@ $(document).ready(function(){
             //get latitude and longitude from geo
             var lat = data[0].lat  
                 var lon = data[0].lon
-                console.log([lat, lon])
                 //get weather info based on lat and lon
                 fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apiKay)
                 .then(function (response){
                     return response.json()
                 })
                 .then(function(data){
-                    console.log(data)
+    
                     //============================CURRENT WEATHER==========================================
                     //OBJECT for current day
                     var currentDay = {}
@@ -88,7 +84,7 @@ $(document).ready(function(){
                     //current humidity
                     var humidity = data.main.humidity   
                     currentDay.humidity = humidity
-                    console.log(currentDay)
+
                     //==================================1 DAY OUT=====================================
                     
                 })
@@ -101,7 +97,6 @@ $(document).ready(function(){
                     var day1 = {}
                     //icon for day 1
                     var day1IconID = data.list[7].weather[0].icon
-                    console.log(day1IconID)
                     var Day1iconURL = "http://openweathermap.org/img/wn/"+day1IconID+"@2x.png"
                     day1.icon = Day1iconURL
                     //temp for day 1
@@ -113,8 +108,74 @@ $(document).ready(function(){
                     //humidity for day 1
                     var day1Humidity = data.list[7].main.humidity
                     day1.humidity = day1Humidity
-                    console.log(day1)
-                   
+                    
+                    //==================================2 DAYs OUT=====================================
+                   //OBJECT for day 1
+                   var day2 = {}
+                   //icon for day 1
+                   var day2IconID = data.list[15].weather[0].icon
+                   var Day2iconURL = "http://openweathermap.org/img/wn/"+day2IconID+"@2x.png"
+                   day2.icon = Day2iconURL
+                   //temp for day 1
+                   var day2Temp = Math.floor(1.8*(data.list[15].main.temp-273)+32)
+                   day2.temp = day2Temp
+                   //wind for day 1
+                   var day2Wind = data.list[15].wind.speed
+                   day2.wind = day2Wind
+                   //humidity for day 1
+                   var day2Humidity = data.list[15].main.humidity
+                   day2.humidity = day2Humidity
+
+                     //==================================3 DAYs OUT=====================================
+                   //OBJECT for day 1
+                   var day3 = {}
+                   //icon for day 1
+                   var day3IconID = data.list[23].weather[0].icon
+                   var Day3iconURL = "http://openweathermap.org/img/wn/"+day3IconID+"@2x.png"
+                   day3.icon = Day3iconURL
+                   //temp for day 1
+                   var day3Temp = Math.floor(1.8*(data.list[23].main.temp-273)+32)
+                   day3.temp = day3Temp
+                   //wind for day 1
+                   var day3Wind = data.list[23].wind.speed
+                   day3.wind = day3Wind
+                   //humidity for day 1
+                   var day3Humidity = data.list[23].main.humidity
+                   day3.humidity = day3Humidity
+
+                      //==================================4 DAYs OUT=====================================
+                   //OBJECT for day 1
+                   var day4 = {}
+                   //icon for day 1
+                   var day4IconID = data.list[31].weather[0].icon
+                   var Day4iconURL = "http://openweathermap.org/img/wn/"+day4IconID+"@2x.png"
+                   day4.icon = Day4iconURL
+                   //temp for day 1
+                   var day4Temp = Math.floor(1.8*(data.list[31].main.temp-273)+32)
+                   day4.temp = day4Temp
+                   //wind for day 1
+                   var day4Wind = data.list[31].wind.speed
+                   day4.wind = day4Wind
+                   //humidity for day 1
+                   var day4Humidity = data.list[31].main.humidity
+                   day4.humidity = day4Humidity
+
+                       //==================================5 DAYs OUT=====================================
+                   //OBJECT for day 1
+                   var day5 = {}
+                   //icon for day 1
+                   var day5IconID = data.list[39].weather[0].icon
+                   var Day5iconURL = "http://openweathermap.org/img/wn/"+day5IconID+"@2x.png"
+                   day5.icon = Day5iconURL
+                   //temp for day 1
+                   var day5Temp = Math.floor(1.8*(data.list[39].main.temp-273)+32)
+                   day5.temp = day5Temp
+                   //wind for day 1
+                   var day5Wind = data.list[39].wind.speed
+                   day5.wind = day5Wind
+                   //humidity for day 1
+                   var day5Humidity = data.list[39].main.humidity
+                   day5.humidity = day5Humidity
                 })
         })      
 
